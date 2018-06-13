41a3d3ae2478d9e1c541f2be6c05c329cb1c6b8c
JK54 <GowJK54@gmail.com>
2018-06-13 16:15:12 +0800

    tail

--- Unix-like/head.c
+++ Unix-like/head.c
@@ -4,16 +4,16 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
-#define COLUMN 11
+#define COLUMN 24
 #define LEN 512
 int main(int argc,char **argv)
 {
 	FILE *stream;
-	char line[COLUMN][LEN];
-	int col = -1,set;
+	char line[LEN];
+	int col = COLUMN;
 	if(argc != 2)
 	{
-		perror("Usage: tail file");
+		perror("Usage: head file");
 		exit(1);
 	}
 	stream = fopen(argv[1],"r");
@@ -22,12 +22,10 @@ int main(int argc,char **argv)
 		perror("Error opening file");
 		exit(1);
 	}
-	while(fgets(line[(++col) % COLUMN],LEN,stream) != NULL);
-	if(col < COLUMN)
-		set = 0;
-	else
-		set = (col + 1) % COLUMN;
-	while(set % COLUMN != col % COLUMN)
-		fputs(line[set++ % COLUMN],stdout);
+	while(col > 0 && fgets(line,512,stream) != NULL )
+	{
+		fputs(line,stdout);
+		col--;
+	}
 	return 0;
 }
