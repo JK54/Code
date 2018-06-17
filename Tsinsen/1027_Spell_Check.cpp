0dbf108f9e6e25c87b185ae84fe72eab83ef34c6
Ash_JK54 <GowJK54@gmail.com>
2018-06-17 14:01:41 +0800

    spell

--- Tsinsen/1027_Spell_Check.cpp
+++ Tsinsen/1027_Spell_Check.cpp
@@ -1,30 +1,75 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
 
 #define LENGTH 100
 #define MAXWORDLEN 20
+//5 situation:1.one less char;2.one more char;3.one wrong char;4.absolutely correct;5.others,just return "NOANSWER"
 void Check(std::string *list,std::string wanted,std::string &result)
 {
-	int i = 0;
+	size_t i,j,k;
 	std::string::iterator p,q;
 	std::string tmp;
-	tmp = list[0];
+	std::string tmpresult;
 	result = "";
-	q = wanted.begin();
-	while(tmp != "")
+	for(i = 0;i < LENGTH;i++)
 	{
+		tmp = list[i];
 		p = tmp.begin();
-		while(*q == *p && p != tmp.end() && q != wanted.end())
-			p++,q++;
-		if( p == tmp.end() && q == wanted.end())
-			result = tmp;
+		q = wanted.begin();
+		if(tmp.length() == wanted.length() - 1)
+		{
+			k = 0;
+			for(j = 0;j < wanted.length();j++)
+			{
+				if(*p == *q)
+					p++,q++,k++;
+				else
+					q++;
+			}
+			if(k == wanted.length() - 1 && tmpresult == "")
+				tmpresult = tmp;
+		}
+		else if(tmp.length() == wanted.length() + 1)
+		{
+			k = 0;
+			for(j = 0;j < tmp.length();j++)
+			{
+				if(*p == *q)
+					p++,q++,k++;
+				else
+					p++;
+			}
+			if(k == wanted.length() && tmpresult == "")
+				tmpresult = tmp;
+		}
 		else
-			tmp = list[++i];
+		{
+			if(tmp == wanted)
+			{
+				result = tmp;
+				return;
+			}
+			else
+			{
+				k = 0;
+				for(j = 0;j < tmp.length();j++)
+				{
+					if(*p == *q)
+						p++,q++,k++;
+					else
+						p++,q++;
+				}
+				if(k == wanted.length() && tmpresult == "")
+					tmpresult = tmp;				
+			}
+		}
 	}
-	if(result == "")
+	if(tmpresult =="")
 		result = "NOANSWER";
+	else
+		result = tmpresult;
 }
-
 int main()
 {
 	int i = 0;
