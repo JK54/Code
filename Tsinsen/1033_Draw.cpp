67e7941c719ca4266bdeefaeb97ae9f5a4925721
JK54 <GowJK54@gmail.com>
2018-06-17 23:22:13 +0800

    38

--- Tsinsen/1033_Draw.cpp
+++ Tsinsen/1033_Draw.cpp
@@ -2,7 +2,37 @@
 
 void draw(int n)
 {
-	int *
+	int i,j;
+	//left,right,up,down ,flag of direction
+	bool l,r,u,d;
+	l = r = u = d = false;
+	int **data = new int *[n];
+	for(i = 0;i < n;i++)
+		data[i] = new int [n];
+	for(i = 1;i <= 2 * n - 1;i++)
+	{
+		if(r == true)
+			r = false,d = true;
+		else if(d == true)
+			d = false,l = true;
+		else if(l == true)
+			l = false,u = true;
+		else if(u == true)
+			u = false,d = true;
+		else
+			//the first round.
+			d = true;
+		//
+		if(r == true || l == true)
+		{
+		
+		}
+		//
+		else if(u == true || d == true)
+		{
+		
+		}
+	}
 }
 
 int main()
---
+++ Tsinsen/1037_Downstair.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int method(int n)
+{
+	if(n == 0)
+		return 1;
+	else if(n < 0)
+		return 0;
+	else
+		return method(n - 1) + method(n - 2) + method(n -3 );
+}
+
+int main()
+{
+	int n,result;;
+	std::cin>>n;
+	if(n < 4 || n >= 20)
+		result = 0;
+	else
+		result = method(n);
+	std::cout<<result<<std::endl;
+}
