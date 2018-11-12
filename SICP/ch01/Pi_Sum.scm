ea9e3345b93ea8dd71ed7da1d4f03b7f9087d3b9
JK54 <GowJK54@gmail.com>
2018-11-12 11:40:41 +0800

    dididi

---
+++ SICP/ch01/Pi_Sum.scm
@@ -0,0 +1,11 @@
+(define (pi-sum a b)
+ 	(define (pi-term x)
+	 	(/ 1.0 (* x (+ x 2))))
+	(define (pi-next x)
+	 	(+ x 4))
+	(sum pi-term a pi-next b))
+(define (sum term a next b)
+ 	(if (> a b)
+	 	0
+		(+ (term a) (sum term (next a) next b))))
+
